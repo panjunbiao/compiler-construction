b13c078a8f36dd3544f6a4212e3d104dc1130b43
panjunbiao <panjunbiao@gmail.com>
2013-07-11 10:09:12 +0800

    Before re-construct the automata classes.

---
+++ src/automata/FAState.java
@@ -0,0 +1,11 @@
+package automata;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: james
+ * Date: 7/10/13
+ * Time: 12:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface FAState {
+}
---
+++ test/automata/NFATest.java
@@ -0,0 +1,107 @@
+package automata;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: james
+ * Date: 7/10/13
+ * Time: 11:11 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NFATest {
+    @Test
+    public void testGetStartState() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAcceptingStates() throws Exception {
+
+    }
+
+    @Test
+    public void testAddAcceptingState() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStateSet() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStateList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStateSet() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEpsilonClosure() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEpsilonClosure() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEpsilonClosure() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testGetImportantStates() throws Exception {
+
+    }
+
+    @Test
+    public void testToDFA() throws Exception {
+
+    }
+}
